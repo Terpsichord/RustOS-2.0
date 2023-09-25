0c95b29ce7f59ab54b84eeee3022b9930d0cae91
Terpsichord <103211495+Terpsichord@users.noreply.github.com>
2023-09-25 18:33:39 +0100

    Fix macros in writer.rs and serial.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() {
     cmd.arg("-bios")
         .arg(ovmf_prebuilt::ovmf_pure_efi())
         .arg("-drive")
-        .arg(format!("format=raw,file={uefi_path}"));
+        .arg(format!("format=raw,file={uefi_path}"))
+        .arg("-serial")
+        .arg("stdio");
 
     println!("{uefi_path}");
     let exit_status = cmd.status().unwrap();
